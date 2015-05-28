041a26ae6a9d812b00a01364039adcad2c36f3f0
mrclk <finsterr@googlemail.com>
2015-05-28 21:12:28 +0200

    tried to fix mail input

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -2,14 +2,14 @@
 
 angular.module('questionnaire')
   .controller('MainCtrl', function ($scope, $firebaseArray) { 
+
       $scope.mail = {
         address : ""
       };
 
-      var ref = new Firebase("https://glowing-fire-9806.firebaseio.com/mails");
-      $scope.mails = $firebaseArray(ref);
-
       $scope.send = function() {
+          var ref = new Firebase("https://glowing-fire-9806.firebaseio.com/mails");
+          $scope.mails = $firebaseArray(ref);
           
           $scope.mails.$add({
             "address": $scope.mail.address
