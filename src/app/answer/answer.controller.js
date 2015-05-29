f435af9659d1d54409829030f9b5f2cef11df07b
mrclk <finsterr@googlemail.com>
2015-05-29 23:33:18 +0200

    - fixed mail input - added first custom css - minor fixes with firebase

--- src/app/answer/answer.controller.js
+++ src/app/answer/answer.controller.js
@@ -3,9 +3,15 @@
 angular.module('questionnaire')
   .controller('AnswerCtrl', function ($scope, $firebaseArray) {
 
+	$scope.loading = true;
+
     var ref = new Firebase("https://glowing-fire-9806.firebaseio.com/questions");
+    ref.on("value", function(snapshot) {}, function(err) {
+	  	$scope.loading = false;
+	    $scope.anyAnswers = false;
+	});
+
     $scope.questions = $firebaseArray(ref);
-    $scope.loading = true;
 
     $scope.questions.$loaded().then(function(questions) {
     	$scope.loading = false;
